86a9ab32ad16c1439fbead22057a04341b42e7e1
hakan karadeniz <hakan.karadeniz@outlook.com>
2022-05-27 11:45:35 +0300

    27.05.2022

--- hakan/exception_handling.cpp
+++ hakan/exception_handling.cpp
@@ -6,18 +6,34 @@ class fault: public exception{
         return "something went wrong";    }
 };
 
+class fault_two: public exception{
+    public: char* hata(){
+        return "another something went wrong";    }
+};
+
+
 class trying {
     public:
         int f(int x){
             if(x == 0){
-                throw fault();      }        }
+                throw fault();}
+            if(x == 5){
+                throw fault_two(); } 
+            }
 };
 
 
 int main(){
     
     trying A;
-    A.f(0);
+    trying B;
 
+    try{
+        
+    B.f(5);
+
+    }catch(exception e){
+        cout << "general fault" << endl;
+    }
     return 0;
 }
